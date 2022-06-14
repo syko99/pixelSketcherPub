11292a4134a8f24fcfbc1de6413fd35a75d20478
syko99 <sykong.yang@hotmail.com>
2022-06-14 15:46:10 -0500

    working sketch pad, still need to implement features like color and grid size change

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
-let sketchGrid = document.querySelector(".sketch__grid");
-let gridMaxWidth = 600;
 
-function createGrid(pixels) {
-    for (let i = 0; i < pixels; i++){
-        for (let j = 0; j < pixels; j++){
-            let panel = document.createElement("div");
-            panel.classList.add("panels");
-            let widthHeight = gridMaxWidth/pixels;
-            panel.setAttribute("style", `width: ${widthHeight}px; height: ${widthHeight}px;`);
-            panel.addEventListener("mouseover", ()=>{
-                let randomRGB = Math.floor(Math.random()*256);
-                let randomRGB2 = Math.floor(Math.random()*256);
-                let randomRGB3 = Math.floor(Math.random()*256);
-                panel.style.backgroundColor = `rgb(${randomRGB}, ${randomRGB2}, ${randomRGB3})`;
-            })
-            panel.addEventListener("click", ()=>{
-                panel.style.backgroundColor = "white";
-            })
-            sketchGrid.appendChild(panel);
-        }
+const DEFAULTSIZE = 16;
+
+
+let sketchGrid = document.querySelector(".sketch__grid");
+
+
+
+let mousedown = false;
+document.body.onmousedown = ()=> (mousedown = true);
+document.body.onmouseup = ()=> (mousedown = false);
+
+
+
+
+
+createGrid(DEFAULTSIZE);
+
+function createGrid(DEFAULTSIZE) {
+    sketchGrid.setAttribute("style", `grid-template-columns: repeat(${DEFAULTSIZE}, 1fr); grid-template-rows: repeat(${DEFAULTSIZE}, 1fr);`);
+    for (let i = 0; i < DEFAULTSIZE*DEFAULTSIZE; i++){
+        let panel = document.createElement("div");
+        panel.classList.add("panels");
+        panel.addEventListener("mouseover", color);
+        panel.addEventListener("mousedown", color)
+        sketchGrid.appendChild(panel);
     }
 }
 
-
-let newGridBtn = document.querySelector("#newGridBtn");
-newGridBtn.addEventListener("click", function gridPrompt(){
-    let pixels = prompt("What size grid would you like? Min: 2  Max: 50");
-    sketchGrid.setAttribute("style", `grid-template-columns:  repeat(${pixels}, 1fr); grid-template-rows: repeat(${pixels}, 1fr);`);
-    if (pixels >= 2 && pixels <= 50){
-        createGrid(pixels);
-    } else {
-        gridPrompt();
+function color(e){
+    if (e.type === "mouseover" && mousedown){
+        e.target.style.backgroundColor = "black";
     }
-});
-
+    if(e.type === "mousedown"){
+        e.target.style.backgroundColor = "black";
+    }else{
+        return;
+    }
+}
 
 
