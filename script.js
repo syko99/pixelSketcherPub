0d349f62c5aba33a65844a1e4afa3c050709bf99
syko99 <sykong.yang@hotmail.com>
2022-06-15 11:06:39 -0500

    added a 'clear' button, working on color picker feature

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 
 const DEFAULTSIZE = 16;
+const DEFAULTCOLOR = "black";
 
+let activeSize = DEFAULTSIZE;
+let activeColor = DEFAULTCOLOR;
+
+let colorPicker = document.querySelector("#colorPicker");
+let clearBtn = document.querySelector("#clearBtn");
 let sketchGrid = document.querySelector(".sketch__grid");
 let gridSize = document.querySelector("#gridSize");
 
 let mousedown = false;
 document.body.onmousedown = ()=> (mousedown = true);
 document.body.onmouseup = ()=> (mousedown = false);
+
+colorPicker.addEventListener("input", ()=>{
+    console.log(e);
+})
+
+clearBtn.addEventListener("click", ()=>{
+    createGrid(activeSize)
+});
+
+gridSize.onchange = (e) => {
+    activeSize = e.target.value;
+    gridSize.labels[0].textContent = `Grid Size: ${activeSize} x ${activeSize}`;
+    createGrid(activeSize);
+};
 
 function createGrid(size) {
     sketchGrid.textContent = "";
@@ -33,7 +53,4 @@ function color(e){
 
 createGrid(DEFAULTSIZE);
 
-gridSize.onchange = (e) => {
-    gridSize.labels[0].textContent = `Grid Size: ${e.target.value} x ${e.target.value}`;
-    createGrid(e.target.value);
-};
+
